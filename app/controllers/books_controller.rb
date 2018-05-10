725a663368cf88c6799e2be837696c563200b7fc
daizo0509 <yuya.ohkura10@gmail.com>
2018-05-10 06:47:23 +0000

    edit update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,18 @@ class BooksController < ApplicationController
   end
 
   def edit
-  	  @book = Post.find(params[:id])
+  	  @book = Book.find(params[:id])
   end
 
   def create
   	  book = Book.new(book_params)
   	  book.save
   	  redirect_to new_book_path
+  end
+  def update
+  	  book = Book.find(params[:id])
+  	  book.update(book_params)
+  	  redirect_to book_path(book.id)
   end
 
   private
