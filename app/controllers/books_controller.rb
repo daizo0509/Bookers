7e294ec9544c46bf2fda90fcee76a96d52dddf86
daizo0509 <yuya.ohkura10@gmail.com>
2018-05-10 05:46:50 +0000

    show&new&index update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def show
+  	  @book = Book.find(params[:id])
   end
 
   def new
+  	  @books =  Book.all
+  	  @book = Book.new
   end
 
   def edit
   end
+
+  def create
+  	  book = Book.new(book_params)
+  	  book.save
+  	  redirect_to new_books_path
+  end
+
+  private
+    def book_params
+    	params.require(:book).permit(:title,:body)
+    end
 end
